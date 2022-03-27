a4c09175671b717c1f9372c8afa595194302652d
mario-tues10 <marholincho2003@gmail.com>
2022-03-27 11:41:36 +0300

    User Service and main page background picture!

---
+++ src/main/java/com/example/votingSystem/services/UserService.java
@@ -0,0 +1,70 @@
+package com.example.votingSystem.services;
+
+
+import com.example.votingSystem.models.User;
+import com.example.votingSystem.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.regex.Pattern;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    public void createUser(String pin){
+        User curr = new User();
+        curr.setPIN(pin);
+        curr.setBirth(createDate(pin));
+        userRepository.save(curr);
+    }
+
+
+    public boolean isAdult(String pin){
+        LocalDate curr = LocalDate.now();
+        LocalDate userYears = createDate(pin);
+        long diff = ChronoUnit.YEARS.between(userYears, curr);
+
+        return diff >= 18;
+    }
+
+    public boolean presentUser(String pin){
+        return userRepository.findByPIN(pin) != null;
+    }
+    
+    private LocalDate createDate(String pin){
+        String born_year = pin.substring(0, 2);
+        String born_month = pin.substring(2, 4);
+        
+        String born_date = pin.substring(4, 6);
+        if(ageParser(born_month).equals("20")){
+            born_year = "19" + born_year;
+        }else if(ageParser(born_month).equals("21")){
+            born_year = "20" + born_year;
+            born_month = String.valueOf((Integer.parseInt(born_month) - 40));
+        }
+
+        if(Pattern.matches("[1-9]", born_month)){
+            born_month = "0" + born_month;
+        }
+         
+        String exactDate = born_date + "-" + born_month + "-" + born_year;
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.parse(exactDate, dateFormat);
+    }
+
+    private String ageParser(String months){
+        if(months.startsWith("4") || months.startsWith("5")){
+            return "21";
+        }else if(months.startsWith("0") || months.startsWith("1")){
+            return "20";
+        }
+        return "wrong";
+    }
+}
